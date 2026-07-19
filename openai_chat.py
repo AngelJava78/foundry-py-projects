3381929825984bf962ab0d3083ca856270347f8c
Angel Javier Valdez <angel_java@hotmail.com>
2026-07-19 05:49:52 +0000

    Added instructions and max_output_tokens

--- openai_chat.py
+++ openai_chat.py
@@ -8,13 +8,19 @@ openai_url = os.getenv("OPENAI_BASE_URL")
 print(f"OPENAI_BASE_URL: {openai_url}")
 model_name="gpt-5-mini"
 openai_client = OpenAI()
+instructions="""
+You are a helpful AI assistant that answers questions clearly and concisely.
+"""
 while True:
     input_text = input("Question: ")
     if input_text.lower() == "exit":
         break;
     response = openai_client.responses.create(
         model = model_name,
-        input = input_text
+        instructions = instructions,
+        input = input_text,
+        # temperature = 0.8,
+        max_output_tokens = 1000
     )
     print("Answer: ")
     print(f"{response.output_text}")
