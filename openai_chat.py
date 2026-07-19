cd221054a9bde626db89268b6537754ed644707b
Angel Javier Valdez <angel_java@hotmail.com>
2026-07-19 05:38:12 +0000

    Implement interactive conversation with OpenAI chatbot

--- openai_chat.py
+++ openai_chat.py
@@ -6,8 +6,22 @@ load_dotenv()
 
 openai_url = os.getenv("OPENAI_BASE_URL")
 print(f"OPENAI_BASE_URL: {openai_url}")
-
+model_name="gpt-5-mini"
 openai_client = OpenAI()
+while True:
+    input_text = input("Question: ")
+    if input_text.lower() == "exit":
+        break;
+    response = openai_client.responses.create(
+        model = model_name,
+        input = input_text
+    )
+    print("Answer: ")
+    print(f"{response.output_text}")
+    print(f"Response ID: {response.id}")
+    print(f"Tokens used: {response.usage.total_tokens}")
+    print(f"Status: {response.status}")
+    print('*'*50)
 
 print(openai_client)
 
